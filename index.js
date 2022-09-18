201177697835c846a5ccb12be2d2bd1478415003
reyno405 <reyno405@gmail.com>
2022-09-18 12:03:51 -0400

    this has update with questins for readme

---
+++ index.js
@@ -0,0 +1,52 @@
+var fileGenerator = require("./fileGenerator");
+var fs = require("fs");
+var inquirer = require('inquirer');
+
+// questions to ask the user //
+let questions = [
+    {
+        type: "input",
+        message: "What is the title of your repository?",
+        name: "title"
+    }, {
+        type: "input",
+        message: "Please give your logo information.",
+        name: "logo"
+    }, {
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "userName"
+    }, {
+        type: "input",
+        message: "Please give your GitHub profile link.",
+        name: "GitHub"
+    }, {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    }, {
+        type: "list",
+        name: "license",
+        message: "Please select which license you would like to use.",
+        choices: [
+            "APACHE 2.O",
+            "BSD 3",
+            "GVL-GPL 3.0",
+            "MIT",
+            "None"
+        ],
+    }, {
+        type: "input",
+        message: "Please describe the repository.",
+        name: "description"
+
+    }, {
+        type: "input",
+        message: "Please state if others can contribute.",
+        name: "contribute"
+    }
+
+];
+
+
+// Function to write to my ReadMe.md file. //
